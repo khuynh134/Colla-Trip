98b5019505f0fed789cefb4f7357a01d78e610c9
khuynh134 <khuynh134@csu.fullerton.edu>
2025-01-20 14:47:21 -0800

    made edits to layout.svelte to remove duplicates from +page.svelte

--- src/routes/loginpage/+page.svelte
+++ src/routes/loginpage/+page.svelte
@@ -1,28 +1,4 @@
-<nav>
-    <a href="/">Welcome To Our Login Page</a>
-    <a href="/">Login</a>
-
-</nav>
 
 <h1>Welcome to Colla-Trip</h1>
-<p>This is our Welcome Landing page</p>
+<p>This is our Login page</p>
 
-<style>
-    nav {
-        display: flex;
-        justify-content: space-around;
-        padding: 1em;
-        background-color: #65dede;
-        border-bottom: 1px solid #35f0ed;
-
-    }
-
-    nav a {
-        text-decoration: none;
-        color: #333;
-    }
-
-    h1, p {
-        text-align: center; 
-    }
-</style>
