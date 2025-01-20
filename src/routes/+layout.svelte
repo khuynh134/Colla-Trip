1e2f663a5a378f12aad867f6a6c205bdc797dce4
khuynh134 <khuynh134@csu.fullerton.edu>
2025-01-20 15:37:05 -0800

    added styles.css to have global formatting for app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,18 @@
 <script lang="ts">
 	import '../app.css';
+
 	let { children } = $props();
 </script>
+
+<svelte:head>
+    <link rel="stylesheet" href="/styles.css">
+    <link rel="stylesheet" href="/output.css">
+</svelte:head>
 
 <nav>
 	<a href="/">Welcome Landing Page</a>
 	<a href="/">Login</a>
 </nav>
 
-<style>
-    nav {
-        display: flex;
-        justify-content: space-around;
-        padding: 1em;
-        background-color: #65dede;
-        border-bottom: 1px solid #35f0ed;
 
-    }
-
-    nav a {
-        text-decoration: none;
-        color: #333;
-    }
-
-    /* h1, p {
-        text-align: center; 
-    } */
-</style>
 {@render children()}
