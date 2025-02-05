5f428ca42dfc3718f6666f6dfb145d383ab7dd6f
khuynh134 <khuynh134@csu.fullerton.edu>
2025-02-05 10:28:45 -0800

    made edits for initialize firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore"; 
 import { getAuth } from "firebase/auth"; 
 
@@ -22,6 +22,6 @@ const firebaseConfig = {
 
 //Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const auth = getAuth(app); 
 
-export { auth }; 
+export { app, auth}; 
