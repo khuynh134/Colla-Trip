472a5cd5b4399d6a3496cb08701f219790e12a1a
khuynh134 <khuynh134@csu.fullerton.edu>
2025-02-16 13:22:48 -0800

    changes to database.js file

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -1,6 +1,6 @@
 import postgres from 'postgres'; //Import the postgres library 
-import { PGCONNECT } from '$env/static/private'; //Import the PGCONNNECT environment variable 
+import { env } from '$env/dynamic/private'; //Import the PGCONNNECT environment variable 
 
-const sql = postgres(PGCONNECT, {}); //Create a new connection to the database using the PGCONNECT environment variable 
+const sql = postgres(env.PGCONNECT); //Create a new connection to the database using the PGCONNECT environment variable 
 
 export default sql; //Export the connection to the database 
