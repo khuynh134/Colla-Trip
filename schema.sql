bb87762478344a972a74401ccbed97eb42a3b322
khuynh134 <khuynh134@csu.fullerton.edu>
2025-03-05 13:24:48 -0800

    added a schema.sql file for database table users

---
+++ schema.sql
@@ -0,0 +1,13 @@
+-- schema.sql 
+
+--Users Table 
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    firebase_uid VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    profile JSONB,
+    roles VARCHAR(50) [] DEFAULT ARRAY['user'] 
+); 
