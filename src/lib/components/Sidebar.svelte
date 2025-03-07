6e380c4dcdc5f033840a6f260588c39612e4d085
khuynh134 <khuynh134@csu.fullerton.edu>
2025-03-06 23:57:42 -0800

    added a sidebar to activitypage

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -135,6 +135,17 @@
               <span class="text-lg">Schedule</span>
             </div>
           </a> 
+
+          <!-- Activity/ Voting  -->
+           <a href="/activitypage" class="flex items-center p-3 hover:bg-blue-600 rounded-lg transition-colors">
+            <div>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-vote"><path d="m9 12 2 2 4-4"/>
+                <path d="M5 7c0-1.1.9-2 2-2h10a2 2 0 0 1 2 2v12H5V7Z"/><path d="M22 19H2"/></svg>
+            </div>
+            <div class="ml-4 {sidebarExtended ? 'opacity-100' : 'opacity-0'} transition-opacity duration-500">
+              <span class="text-lg">Activity</span>
+            </div>
+          </a>
          
       </div>
     </div>
