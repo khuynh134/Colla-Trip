e95e179ff20b03f77085abdfd330d3488d744ce2
khuynh134 <khuynh134@csu.fullerton.edu>
2025-04-09 11:50:58 -0700

    updated neon-setup.sql to add trips and trip_members table into database

--- neon-setup.sql
+++ neon-setup.sql
@@ -45,3 +45,24 @@ CREATE TABLE users(
 CREATE UNIQUE INDEX users_firebase_uid_key ON users USING btree (firebase_uid);
 CREATE UNIQUE INDEX users_email_key ON users USING btree (email);
 CREATE UNIQUE INDEX users_username_key ON users USING btree (username);
+
+CREATE TABLE trip_members(
+    id SERIAL NOT NULL,
+    trip_id integer,
+    user_id integer,
+    PRIMARY KEY(id),
+    CONSTRAINT trip_members_trip_id_fkey FOREIGN key(trip_id) REFERENCES trips(id),
+    CONSTRAINT trip_members_user_id_fkey FOREIGN key(user_id) REFERENCES users(id)
+); 
+
+CREATE TABLE trips(
+    id SERIAL NOT NULL,
+    name varchar(255) NOT NULL,
+    location text,
+    start_date date,
+    end_date date,
+    budget numeric,
+    created_at timestamp without time zone DEFAULT now(),
+    updated_at timestamp without time zone DEFAULT now(),
+    PRIMARY KEY(id)
+);
