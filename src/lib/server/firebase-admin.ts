b4d1ba91f2492eb52d8ef6a8db39bfc891270e8e
cristinahh <123323452+cristinahh@users.noreply.github.com>
2025-04-28 08:18:52 -0700

    keys

--- src/lib/server/firebase-admin.ts
+++ src/lib/server/firebase-admin.ts
@@ -1,19 +1,16 @@
 // src/lib/server/firebase-admin.ts
 import { cert, initializeApp, getApps } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
-import { FIREBASE_ADMIN_KEY } from '$env/static/private';
 import { env } from '$env/dynamic/private'; 
-
-const serviceAccount = JSON.parse(FIREBASE_ADMIN_KEY);
 
 
 const firebaseAdmin = getApps().length > 0 ?
 getApps()[0] :
 initializeApp({
     credential: cert({
-        projectId: env.FIREBASE_PROJECT_ID,
-        clientEmail: env.FIREBASE_CLIENT_EMAIL,
-        privateKey: env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // 🔥 fix \n issues
+        projectId: env.PROJECT_ID,
+        clientEmail: env.CLIENT_EMAIL,
+        privateKey: env.PRIVATE_KEY.replace(/\\n/g, '\n'), 
       }),
     databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
 });
