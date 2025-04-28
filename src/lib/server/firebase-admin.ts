eb7ca540f1e37fd20f5c8677be00ebc5a97afe3b
cristinahh <123323452+cristinahh@users.noreply.github.com>
2025-04-28 08:23:19 -0700

    fix

--- src/lib/server/firebase-admin.ts
+++ src/lib/server/firebase-admin.ts
@@ -3,16 +3,15 @@ import { cert, initializeApp, getApps } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { env } from '$env/dynamic/private'; 
 
-
-const firebaseAdmin = getApps().length > 0 ?
-getApps()[0] :
-initializeApp({
-    credential: cert({
+const firebaseAdmin = getApps().length > 0
+  ? getApps()[0]
+  : initializeApp({
+      credential: cert({
         projectId: env.PROJECT_ID,
         clientEmail: env.CLIENT_EMAIL,
         privateKey: env.PRIVATE_KEY.replace(/\\n/g, '\n'), 
       }),
-    databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
-});
+      // 
+    });
 
 export const adminAuth = getAuth(firebaseAdmin);
