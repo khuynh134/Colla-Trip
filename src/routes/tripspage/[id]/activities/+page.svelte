2033e518ebda5f944dba7bf6ba1b1362f711617a
cristinahh <123323452+cristinahh@users.noreply.github.com>
2025-04-29 09:47:41 -0700

    tsconfig

--- src/routes/tripspage/[id]/activities/+page.svelte
+++ src/routes/tripspage/[id]/activities/+page.svelte
@@ -11,7 +11,7 @@
     import 'flatpickr/dist/flatpickr.min.css'; 
    
 
-    let userSelectedActivityDate: Date | null = new Date();
+    
     let dateInputRef: HTMLInputElement | null = null;
 
     onMount(() => {
